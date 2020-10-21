14247fbebd3ace022671555bc888ec952933394d
Konrad <gornikonrad@gmail.com>
2020-10-21 10:23:32 +0200

    Updates

---
+++ precache-manifest.d51d70c63c771deb4a27855f976086d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9234aa3975cc024a8f442bca0cbf5578",
+    "url": "/NotesApp/index.html"
+  },
+  {
+    "revision": "50965b20ba9fc7f90f32",
+    "url": "/NotesApp/static/css/2.11d7490e.chunk.css"
+  },
+  {
+    "revision": "50965b20ba9fc7f90f32",
+    "url": "/NotesApp/static/js/2.a79821b6.chunk.js"
+  },
+  {
+    "revision": "735564a604c3e51579a3a6f6a8ee68a0",
+    "url": "/NotesApp/static/js/2.a79821b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fc7cc63aa26fe49a62",
+    "url": "/NotesApp/static/js/main.33380488.chunk.js"
+  },
+  {
+    "revision": "76b36b1cde110592502d",
+    "url": "/NotesApp/static/js/runtime-main.d7c01b6d.js"
+  },
+  {
+    "revision": "02b4fb0de0721b3fd833537c61ec3600",
+    "url": "/NotesApp/static/media/img_fullhd.02b4fb0d.jpg"
+  }
+]);
